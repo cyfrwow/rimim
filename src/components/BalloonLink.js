a1dd2912939b27d15de45e55ba399d5a9edd0ec3
Kamaraj P <kamaraju.p@postman.com>
2021-07-19 17:48:23 +0530

    delete link done

--- src/components/BalloonLink.js
+++ src/components/BalloonLink.js
@@ -8,6 +8,7 @@ import {
     ELEMENT_LINK,
     getAbove,
     upsertLinkAtSelection,
+    getSelectionText,
 } from '@udecode/slate-plugins';
 import cx from 'classnames';
 import { Transforms } from 'slate';
@@ -20,6 +21,7 @@ const BalloonLink = () => {
     const [show, setShow] = useState(false);
     const [value, setValue] = useState('');
     const [selection, setSelection] = useState(null);
+    const [isEditMode, setIsEditMode] = useState(true);
 
     const isLinkOpen = useEditorStore((state) => state.isLinkOpen);
     const setIsLinkOpen = useEditorStore((state) => state.setIsLinkOpen);
@@ -31,14 +33,15 @@ const BalloonLink = () => {
     useEffect(() => {
         // console.log(isLinkOpen, editorSelection);
         setShow((prevState) => isLinkOpen);
+        setPositionAtSelection(ref.current, 'top');
     }, [isLinkOpen]);
 
     useEffect(() => {
         // console.log(show, editor?.selection);
-        if (!editor || show || !editor.selection) return;
+        if (!editor || show || !editor.selection) {
+            return;
+        }
         //to check if selection on editor is a link
-
-        // if (show && !editor.selection) return;
         const active =
             editor !== null &&
             editor !== void 0 &&
@@ -49,7 +52,6 @@ const BalloonLink = () => {
                 },
             });
         if (!active) return;
-        // console.log(editor.selection);
         if (active && !show) {
             //get parent of the selection
             const linkNode = getAbove(editor, {
@@ -60,6 +62,7 @@ const BalloonLink = () => {
             //get the url value of the link
             if (linkNode) {
                 setValue(linkNode[0].url);
+                setIsEditMode(false);
             }
             //move cursor to end of word
             Transforms.move(editor, {
@@ -67,7 +70,7 @@ const BalloonLink = () => {
                 unit: 'word',
                 edge: 'start',
             });
-            //select the entire word
+            // select the entire word
             Transforms.select(editor, {
                 anchor: {
                     path: editor.selection.anchor.path,
@@ -78,19 +81,11 @@ const BalloonLink = () => {
                     offset: editor.selection.anchor.offset,
                 },
             });
+            console.log('after select', editor.selection);
             //save the selection
-            setSelection({
-                anchor: {
-                    path: editor.selection.anchor.path,
-                    offset: 0,
-                },
-                focus: {
-                    path: editor.selection.focus.path,
-                    offset: editor.selection.anchor.offset,
-                },
-            });
+            setSelection(editor.selection);
             //move the ref component to the selection point
-            setPositionAtSelection(ref.current);
+            setPositionAtSelection(ref.current, 'top');
             //show the link component
             setIsLinkOpen(true);
         }
@@ -118,6 +113,17 @@ const BalloonLink = () => {
         setValue('');
         setShow(false);
         setSelection(null);
+        setIsEditMode(true);
+    };
+
+    const removeLink = () => {
+        Transforms.select(editor, selection);
+        const text = getSelectionText(editor);
+        Transforms.removeNodes(editor, {
+            match: (n) => n.type === ELEMENT_LINK,
+        });
+        Transforms.insertText(editor, text);
+        handleClose();
     };
     return (
         <PortalBody>
@@ -125,19 +131,46 @@ const BalloonLink = () => {
                 className={cx('link__container', show ? 'show' : '')}
                 ref={ref}
             >
-                <input
-                    className='link__input'
-                    placeholder='URL'
-                    value={value}
-                    autoFocus
-                    onChange={(e) => setValue(e.target.value)}
-                />
-                <span className='link__button' onClick={(e) => handleClick(e)}>
-                    add
-                </span>
-                <span className='link__button' onClick={handleClose}>
-                    X
-                </span>
+                {isEditMode ? (
+                    <>
+                        <input
+                            className='link__input'
+                            placeholder='URL'
+                            value={value}
+                            autoFocus
+                            onChange={(e) => setValue(e.target.value)}
+                        />
+                        <span
+                            className='link__button'
+                            onClick={(e) => handleClick(e)}
+                        >
+                            add
+                        </span>
+                        <span className='link__button' onClick={handleClose}>
+                            X
+                        </span>
+                    </>
+                ) : (
+                    <>
+                        <a
+                            target='_blank'
+                            rel='noreferrer'
+                            className='link__label'
+                            href={value}
+                        >
+                            {value}
+                        </a>
+                        <span
+                            className='link__button'
+                            onClick={() => setIsEditMode(true)}
+                        >
+                            edit
+                        </span>
+                        <span className='link__button' onClick={removeLink}>
+                            del
+                        </span>
+                    </>
+                )}
             </div>
         </PortalBody>
     );
